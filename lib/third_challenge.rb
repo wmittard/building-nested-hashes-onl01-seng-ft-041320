7ed248da84691507b4828611777af85d7751a38f
William Mittard <billymittard@ymail.com>
2020-04-17 17:16:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,12 +1,8 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
+      :patriarch => {:name "Lord Montague", :age "53"},
+      :matriarch => {:name "Lady Montague", :age "54"},
       :hero => {
         #your key/value pairs here
         },
